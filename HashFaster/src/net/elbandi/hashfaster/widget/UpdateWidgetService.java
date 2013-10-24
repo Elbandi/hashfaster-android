7d065e535b88e7b09b72e1bd967d48f82e01cd9b
Elbandi <elso.andras@gmail.com>
2013-10-25 00:42:13 +0200

    Refactor widget service

--- HashFaster/src/net/elbandi/hashfaster/widget/UpdateWidgetService.java
+++ HashFaster/src/net/elbandi/hashfaster/widget/UpdateWidgetService.java
@@ -3,10 +3,9 @@ package net.elbandi.hashfaster.widget;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
-import android.text.InputFilter.LengthFilter;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
@@ -27,53 +26,53 @@ public class UpdateWidgetService extends Service {
 	@Override
 	public void onStart(Intent intent, int startId) {
 		Log.i(LOG, "UpdateWidgetService: onStart() called");
-		// Create some random data
+		final Context ctx = this.getApplicationContext();
+		final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(ctx);
+		final int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
 
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
-		int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
 		Log.w(LOG, "From Intent: " + String.valueOf(allWidgetIds.length));
 
 		if (allWidgetIds.length == 0) {
 			return;
 		}
 
-		for (int widgetId : allWidgetIds) {
+		refreshListener = new RefreshListener() {
+			@Override
+			public void onRefresh() {
+				Log.w(LOG, "onRefresh() called");
+				final RemoteViews remoteViews = new RemoteViews(ctx.getPackageName(), R.layout.widget_layout);
+				mMiner = MinerManager.getInstance().getMiner();
 
-			final RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_layout);
-
-			refreshListener = new RefreshListener() {
-				@Override
-				public void onRefresh() {
-					Log.w(LOG, "onRefresh() called");
-					mMiner = MinerManager.getInstance().getMiner();
+				int count = 0, active = 0;
+				for (Worker w : MinerManager.getInstance().getMiner().getWorkers()) {
+					count++;
+					if (w.hashrate > 0)
+						active++;
 				}
+
+				remoteViews.setTextViewText(R.id.widget_hashrate, mMiner.total_hashrate + " Kh/s");
+				remoteViews.setTextViewText(R.id.widget_round_shares, mMiner.round_shares + "/" + mMiner.round_shares_invalid);
+				remoteViews.setTextViewText(R.id.widget_active_workers, active + "/" + count);
+
+				for (int widgetId : allWidgetIds) {
+
+					// Register an onClickListener
+					Intent clickIntent = new Intent(ctx, MyWidgetProvider.class);
+
+					clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+					clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+
+					PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+					remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
+					appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
+				}
+				Toast.makeText(getApplicationContext(), "HashFaster: Updated stats", Toast.LENGTH_LONG).show();
 			};
+		};
 
-			int count = 0, active = 0;
-			for (Worker w : mMiner.getWorkers()) {
-				count++;
-				if (w.hashrate > 0)
-					active++;
-			}
-			remoteViews.setTextViewText(R.id.widget_hashrate, mMiner.total_hashrate + " Kh/s");
-			remoteViews.setTextViewText(R.id.widget_round_shares, mMiner.round_shares + "/" + mMiner.round_shares_invalid);
-			remoteViews.setTextViewText(R.id.widget_active_workers, active + "/" + count);
-
-			new GetMinerDataTask(getApplicationContext(), refreshListener).execute();
-			new GetWorkerDataTask(getApplicationContext(), refreshListener).execute();
-
-			// Register an onClickListener
-			Intent clickIntent = new Intent(this.getApplicationContext(), MyWidgetProvider.class);
-
-			clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-			clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
-
-			PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-			remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
-			appWidgetManager.updateAppWidget(widgetId, remoteViews);
-
-			Toast.makeText(getApplicationContext(), "HashFaster: Updated stats", Toast.LENGTH_LONG).show();
-		}
+		new GetMinerDataTask(ctx, refreshListener).execute();
+		new GetWorkerDataTask(ctx, refreshListener).execute();
 		// stopSelf();
 	}
 
