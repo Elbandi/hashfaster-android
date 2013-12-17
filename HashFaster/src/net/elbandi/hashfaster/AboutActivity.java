2897f976ecd6edcfc5839016d6dd2e32178ee0f5
Elbandi <elso.andras@gmail.com>
2013-12-17 15:04:45 +0100

    qrcode in about activity

--- HashFaster/src/net/elbandi/hashfaster/AboutActivity.java
+++ HashFaster/src/net/elbandi/hashfaster/AboutActivity.java
@@ -1,22 +1,28 @@
 package net.elbandi.hashfaster;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
+
+import de.schildbach.wallet.litecoin.integration.android.LitecoinIntegration;
 import net.elbandi.hashfaster.R;
+import net.elbandi.hashfaster.utils.BitmapUtils;
 
 public class AboutActivity extends SherlockActivity {
-	Button mSave, mRaw, mRefresh, mDonate;
-	TextView mUsername, mRewards, mRoundEstimate, mHashrate, mPayoutHistory, mRoundShares, mTimestamp, mAddress;
-	EditText mAPIKey;
+	private static final int REQUEST_CODE = 0;
+	Button mDonate;
+	TextView mAddress;
+	ImageView img;
 
 	@Override
 	protected void onCreate(android.os.Bundle savedInstanceState) {
@@ -48,6 +54,16 @@ public class AboutActivity extends SherlockActivity {
 
 			}
 		});
+		OnClickListener donatedclick = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				LitecoinIntegration.requestForResult(AboutActivity.this, REQUEST_CODE, mAddress.getText().toString());
+			}
+		};
+		mAddress.setOnClickListener(donatedclick);
+		img = (ImageView) findViewById(R.id.request_coins_qr);
+		img.setImageBitmap(BitmapUtils.getQRCodeBitmap(mAddress.getText().toString(), 300));
+		img.setOnClickListener(donatedclick);
 	}
 
 	@Override
@@ -57,5 +73,36 @@ public class AboutActivity extends SherlockActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_CODE) {
+			if (resultCode == Activity.RESULT_OK) {
+/*
+				final String txHash = LitecoinIntegration.transactionHashFromResult(data);
+				if (txHash != null) {
+					final SpannableStringBuilder messageBuilder = new SpannableStringBuilder("Transaction hash:\n");
+					messageBuilder.append(txHash);
+					messageBuilder.setSpan(new TypefaceSpan("monospace"), messageBuilder.length() - txHash.length(), messageBuilder.length(),
+							Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+					donateMessage.setText(messageBuilder);
+					donateMessage.setVisibility(View.VISIBLE);
+				}
+
+				donateButton.setEnabled(false);
+				donateButton.setText("Already donated");
+*/
+				Toast.makeText(this, "Thank you!", Toast.LENGTH_LONG).show();
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				Toast.makeText(this, "Cancelled.", Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(this, "Unknown result.", Toast.LENGTH_LONG).show();
+			}
+		} else {
+			super.onActivityResult(requestCode, resultCode, data);
+
+		}
 	}
 }
